5920ac61f182a108afc7081d178a0ddcb890ac33
Shubham Doharey <shubham.doharey@gmail.com>
2019-02-15 02:02:10 +0530

    Update settings.py

--- ecell/settings.py
+++ ecell/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
